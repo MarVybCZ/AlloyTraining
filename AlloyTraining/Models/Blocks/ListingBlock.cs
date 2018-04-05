e3f579332214ddf7f1678b4752ba4725b9ed7f3d
Martin Vybíral <vybiralm81@gmail.com>
2018-04-05 16:38:16 +0200

    E1

---
+++ AlloyTraining/Models/Blocks/ListingBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "ListingBlock", GUID = "62711985-02f0-4ec0-a0f0-7f102e23e808", Description = "")]
+    public class ListingBlock : BlockData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+
+        [CultureSpecific]
+        [Display(Name = "Heading", Description = "", GroupName = SystemTabNames.Content, Order = 100)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "RootPage", Description = "", GroupName = SystemTabNames.Content, Order = 200)]
+        public virtual PageReference RootPage { get; set; }
+    }
+}
---
+++ AlloyTraining/Models/ViewModels/ListingBlockModel.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class ListingBlockModel
+    {
+        public virtual string Heading { get; set;  }
+        public virtual IEnumerable<PageData> Items { get; set; }
+    }
+}
