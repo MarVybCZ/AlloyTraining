ed2d015d09434b91dd0619619397fd3e84d1cb11
Martin Vybíral <vybiralm81@gmail.com>
2018-04-05 16:01:49 +0200

    D3

---
+++ AlloyTraining/Models/ViewModels/BlockEditPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AlloyTraining.Models.Pages;
+using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class BlockEditPageViewModel : IPageViewModel<SitePageData>
+    {
+        public SitePageData CurrentPage { get; set; }
+        public PreviewBlock previewBlock {get; set;}
+
+        public BlockEditPageViewModel(PageData page, IContent content)
+        {
+            previewBlock = new PreviewBlock(page, content);
+
+            CurrentPage = page as SitePageData;
+        }
+    }
+}
