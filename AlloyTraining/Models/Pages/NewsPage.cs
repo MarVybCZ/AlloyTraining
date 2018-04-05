f6eee1942ae5a16e3c92c2a6c7db59d9ca9c9da7
Martin Vybíral <vybiralm81@gmail.com>
2018-04-05 17:07:37 +0200

    E2

---
+++ AlloyTraining/Models/Pages/NewsPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AlloyTraining.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "NewsPage", GUID = "f53a116a-c17f-4045-9c55-5f1e28a2aa2c", Description = "")]
+    public class NewsPage : StandardPage
+    {        
+        [Display(Name = "Main listing", Description = "", GroupName = SystemTabNames.Content, Order = 315)]
+        public virtual ListingBlock MainListing { get; set; }
+    }
+}
